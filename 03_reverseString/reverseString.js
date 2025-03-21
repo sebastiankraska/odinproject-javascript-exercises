46e1242d4da2acc90654152afae2e73e53a9f961
Sebastian Kraska <140939975+sebastiankraska@users.noreply.github.com>
2025-03-21 08:53:54 +0100

    Solution 03 partial: New solution with const of string - easier to read than the first solution but (I wonder if the performance is bad when the string is very long)

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,9 +1,16 @@
 const reverseString = function(string) {
     let reverseString = '';
+
+    // new solution:
+    for (const char of string) {
+        reverseString = char + reverseString;
+    }
+    
     // looping through the string from the end (!) to the beginning:
-    for (let i = string.length - 1; i >= 0; i--) {
-        reverseString += string[i];
-    };
+    // for (let i = string.length - 1; i >= 0; i--) {
+    //     reverseString += string[i];
+    // };
+
     return reverseString;
 };
 
