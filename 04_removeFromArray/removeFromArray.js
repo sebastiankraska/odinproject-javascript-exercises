9e9cbcca410017a367335a81203ba2da1c9b4e1a
Sebastian Kraska <140939975+sebastiankraska@users.noreply.github.com>
2025-03-21 19:08:04 +0100

    solution 4 partial: pseudo code

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,8 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, valuesToRemove) {
+    // iterate through every element of array
+        // iterate through every element from valuesToRemove    
+            // compare with "==" in order to only remove same type (last test)
+                // way a: remove matched items from array / way b: or add non-matched items to a new array
 };
 
 // Do not edit below this line
