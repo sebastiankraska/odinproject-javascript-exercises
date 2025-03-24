030a4d7dc50345c71df68226e37e3e656e1ab701
Sebastian Kraska <140939975+sebastiankraska@users.noreply.github.com>
2025-03-24 08:11:15 +0100

    Solution 05 sumAll and all tests enabled

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,19 @@
-const sumAll = function() {
+const sumAll = function(first, second) {
+    if (!Number.isInteger(first) || !Number.isInteger(second)) { 
+        return 'ERROR'; // error for non-integer input
+    };
+    if (first < 0 || second < 0) {
+        return 'ERROR'; // error for negative input
+    };
+    // making it work for larger number first (sumAll(123, 1)):
+    const lower = Math.min(first, second);
+    const higher = Math.max(first, second);
 
+    let result = 0;
+    for (let i = lower; i <= higher; i++) {
+        result += i;
+    };
+    return result;
 };
 
 // Do not edit below this line
