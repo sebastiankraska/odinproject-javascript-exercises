51725cbb617828a9bac0e6beff1e52f4054c7b98
Sebastian Kraska <140939975+sebastiankraska@users.noreply.github.com>
2025-03-25 07:46:56 +0100

    comments on some of my solutions compared to TOPs solutions

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,11 @@
 const convertToCelsius = function(fahrenheit) {
-  return Number(((fahrenheit - 32) * 5 / 9).toFixed(1));
+  // return Number(((fahrenheit - 32) * 5 / 9).toFixed(1));
+  // not recommended, see solution
 };
 
 const convertToFahrenheit = function(celsius) {
-  return Number((celsius * 9 / 5 + 32).toFixed(1));
+ //  return Number((celsius * 9 / 5 + 32).toFixed(1));
+    // not recommended, see solution
 };
 
 // Do not edit below this line
